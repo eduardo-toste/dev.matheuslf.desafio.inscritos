71edd4f7bf8a7c68b69d64d364e6906bda4fb124
eduardo-toste <duciochetti@gmail.com>
2025-10-22 00:47:07 -0300

    feat: criando metodos de post e get para project

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/service/ProjectService.java
@@ -0,0 +1,30 @@
+package dev.matheuslf.desafio.inscritos.service;
+
+import dev.matheuslf.desafio.inscritos.dto.CreateProjectRequest;
+import dev.matheuslf.desafio.inscritos.dto.ProjectResponse;
+import dev.matheuslf.desafio.inscritos.mapper.ProjectMapper;
+import dev.matheuslf.desafio.inscritos.repository.ProjectRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
+
+    public ProjectService(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+        this.projectRepository = projectRepository;
+        this.projectMapper = projectMapper;
+    }
+
+    public void criarProjeto(CreateProjectRequest request) {
+        projectRepository.save(projectMapper.toEntity(request));
+    }
+
+    public Page<ProjectResponse> buscarProjetos(Pageable pageable) {
+        return projectRepository.findAll(pageable)
+                .map(projectMapper::toResponse);
+    }
+}
