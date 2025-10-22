23bf5562dc4ce3773c74c4d13d39e7ea6f742e43
eduardo-toste <duciochetti@gmail.com>
2025-10-22 12:36:45 -0300

    feat: criando task mapper utilizando MapStruct

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/mapper/TaskMapper.java
@@ -0,0 +1,22 @@
+package dev.matheuslf.desafio.inscritos.mapper;
+
+import dev.matheuslf.desafio.inscritos.dto.CreateTaskRequest;
+import dev.matheuslf.desafio.inscritos.dto.TaskResponse;
+import dev.matheuslf.desafio.inscritos.model.Project;
+import dev.matheuslf.desafio.inscritos.model.Task;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface TaskMapper {
+
+    TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "request.description", target = "description")
+    Task toEntity(CreateTaskRequest request, Project project);
+
+    @Mapping(source = "project.id", target = "projectId")
+    TaskResponse toResponse(Task task);
+}
