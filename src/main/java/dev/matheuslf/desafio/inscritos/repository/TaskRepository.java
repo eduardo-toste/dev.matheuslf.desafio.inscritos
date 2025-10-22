09328c8267dbec93cf84aedec174abbd6f9d979d
eduardo-toste <duciochetti@gmail.com>
2025-10-22 12:36:13 -0300

    feat: criando metodos de service para interagir com a entidade de tarefas

--- src/main/java/dev/matheuslf/desafio/inscritos/repository/TaskRepository.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/repository/TaskRepository.java
@@ -1,7 +1,28 @@
 package dev.matheuslf.desafio.inscritos.repository;
 
 import dev.matheuslf.desafio.inscritos.model.Task;
+import dev.matheuslf.desafio.inscritos.model.enums.Priority;
+import dev.matheuslf.desafio.inscritos.model.enums.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @Query("""
+        SELECT t FROM Task t
+            WHERE (:status IS NULL OR t.status = :status)
+              AND (:priority IS NULL OR t.priority = :priority)
+              AND (:projectId IS NULL OR t.project.id = :projectId)
+    """)
+    Page<Task> findByFilters(Pageable pageable,
+                             @Param("status") Status status,
+                             @Param("priority") Priority priority,
+                             @Param("projectId") Long projectId
+    );
+
 }
