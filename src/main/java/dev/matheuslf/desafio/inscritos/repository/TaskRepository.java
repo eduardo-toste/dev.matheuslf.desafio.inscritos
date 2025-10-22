dd85a6c7cd84b1ea4cbc7cf8d87643726438a76f
eduardo-toste <duciochetti@gmail.com>
2025-10-22 00:46:25 -0300

    feat: criando repositories para as entidades de projeto e tasks

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package dev.matheuslf.desafio.inscritos.repository;
+
+import dev.matheuslf.desafio.inscritos.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
