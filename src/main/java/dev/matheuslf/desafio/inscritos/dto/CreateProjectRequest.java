68113988d95cd938b9001056501ad36d21011183
eduardo-toste <duciochetti@gmail.com>
2025-10-22 00:45:53 -0300

    feat: criando dto para requisição de criar projeto

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/dto/CreateProjectRequest.java
@@ -0,0 +1,18 @@
+package dev.matheuslf.desafio.inscritos.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Date;
+
+public record CreateProjectRequest(
+
+        @NotBlank(message = "Nome é obrigatório")
+        @Size(min = 3, max = 100)
+        String name,
+        String description,
+        Date startDate,
+        Date endDate
+
+) {
+}
