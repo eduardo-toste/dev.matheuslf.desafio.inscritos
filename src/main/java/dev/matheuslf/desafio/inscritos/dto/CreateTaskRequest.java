e6f6f2adad6ba109bbbbf79fb3aa53eda401729a
eduardo-toste <duciochetti@gmail.com>
2025-10-22 12:35:09 -0300

    feat: criando dto para request de criação de tarefa

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/dto/CreateTaskRequest.java
@@ -0,0 +1,31 @@
+package dev.matheuslf.desafio.inscritos.dto;
+
+import dev.matheuslf.desafio.inscritos.model.enums.Priority;
+import dev.matheuslf.desafio.inscritos.model.enums.Status;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Date;
+
+public record CreateTaskRequest(
+
+        @NotBlank
+        @Size(min = 5, max = 150)
+        String title,
+
+        String description,
+
+        @Enumerated(EnumType.STRING)
+        Status status,
+
+        @Enumerated(EnumType.STRING)
+        Priority priority,
+
+        Date dueDate,
+
+        Long projectId
+
+) {
+}
