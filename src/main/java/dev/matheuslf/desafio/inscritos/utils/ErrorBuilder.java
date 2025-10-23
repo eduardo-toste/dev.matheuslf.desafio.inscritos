d471718b730d61dcc8bc05e1a22ecfa9512f7931
eduardo-toste <duciochetti@gmail.com>
2025-10-23 00:02:53 -0300

    feat: criando utilitario para construir erros

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/utils/ErrorBuilder.java
@@ -0,0 +1,24 @@
+package dev.matheuslf.desafio.inscritos.utils;
+
+import dev.matheuslf.desafio.inscritos.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public class ErrorBuilder {
+
+    public ErrorBuilder() {
+    }
+
+    public static ResponseEntity<ErrorResponse> build(HttpStatus status, String message, String path) {
+        ErrorResponse error = ErrorResponse.from(status, message, path);
+        return ResponseEntity.status(status).body(error);
+    }
+
+    public static ResponseEntity<ErrorResponse> build(HttpStatus status, String message, String path, List<String> validationError) {
+        ErrorResponse error = ErrorResponse.from(status, message, path, validationError);
+        return ResponseEntity.status(status).body(error);
+    }
+
+}
