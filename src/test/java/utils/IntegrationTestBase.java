700689ccaf0f70d94d07c2f1ee33bd11eefe4e21
eduardo-toste <duciochetti@gmail.com>
2025-10-27 08:06:38 -0300

    test: criando configuração base para testes de integração

---
+++ src/test/java/utils/IntegrationTestBase.java
@@ -0,0 +1,29 @@
+package utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.matheuslf.desafio.inscritos.repository.ProjectRepository;
+import dev.matheuslf.desafio.inscritos.repository.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public abstract class IntegrationTestBase {
+
+    @Autowired protected MockMvc mockMvc;
+    @Autowired protected ObjectMapper objectMapper;
+    @Autowired protected ProjectRepository projectRepository;
+    @Autowired protected TaskRepository taskRepository;
+
+    @BeforeEach
+    void setup() {
+        taskRepository.deleteAll();
+        projectRepository.deleteAll();
+    }
+
+}
