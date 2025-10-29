b6649318d4e2aa99e4ab31c448083c6336070fc6
eduardo-toste <duciochetti@gmail.com>
2025-10-29 10:26:21 -0300

    config: liberando endpoint do swagger para acesso no security

--- src/main/java/dev/matheuslf/desafio/inscritos/config/security/SecurityConfig.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/config/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
